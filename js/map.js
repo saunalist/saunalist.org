f66ca6a45bbecb9ab715eea59914b19a15d30f9c
saunalist <webmaster@saunalist.org>
2024-04-15 20:29:52 -0400

    update

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var map = L.map('map').setView([0, 0], 2);
+    var map = L.map('map').setView([0, 0], 1);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
